87fe69bc15ea0f1431efa073fd81113f50653c74
Net-AI-Git <ntitz19@gmail.com>
2026-08-23 19:30:19 +0300

    Update environment configuration and dependencies to support OpenAI integration. Replace AzureOpenAI with OpenAI client in repositories, adjust model parameters, and enhance logging. Add new dependencies for OpenAI and related libraries.

--- project/src/repositories/embeddings_repository.py
+++ project/src/repositories/embeddings_repository.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from openai import AzureOpenAI
+from openai import OpenAI
 
 from .opensearch_repository import OpenSearchRepository
 
@@ -10,18 +10,17 @@ load_dotenv()
 
 class OpenAIEmbeddingsRepository:
 
-    client = AzureOpenAI(api_key=os.getenv("AZURE_OPENAI_EMBEDDINGS_API_KEY"), api_version=os.getenv("AZURE_OPENAI_EMBEDDINGS_API_VERSION"), azure_endpoint=os.getenv("AZURE_OPENAI_EMBEDDINGS_ENDPOINT"))
-    embeddings_dim = int(os.getenv("EMBEDDINGS_DIMENSIONS")) if os.getenv("EMBEDDINGS_DIMENSIONS") else 0
-    model_deployment_name = os.getenv("AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME")
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"), base_url=os.getenv("OPENAI_BASE_URL"))
+    model_name = os.getenv("OPENAI_EMBEDDING_MODEL")
 
     @staticmethod
     def generate_embeddings(task_data, flow_id):
-        OpenSearchRepository.log_event(status="STARTING", content=task_data, flow_id=flow_id)
+        OpenSearchRepository.log_event(status="STARTING", content=task_data, flow_id=flow_id, level="INFO")
         embeddings_vector = []
         try:
             text_to_embed = task_data["text"]
-            embeddings_vector = OpenAIEmbeddingsRepository.client.embeddings.create(input=text_to_embed, dimensions=OpenAIEmbeddingsRepository.embeddings_dim, model=OpenAIEmbeddingsRepository.model_deployment_name).data[0].embedding
+            embeddings_vector = OpenAIEmbeddingsRepository.client.embeddings.create(input=text_to_embed, model=OpenAIEmbeddingsRepository.model_name, encoding_format="float").data[0].embedding
         except Exception as err:
-            OpenSearchRepository.log_event(status="ERROR", content={"error": repr(err), "task_data": task_data}, flow_id=flow_id)
-        OpenSearchRepository.log_event(status="FINISHED", content=task_data, flow_id=flow_id)
+            OpenSearchRepository.log_event(status="ERROR", content={"error": repr(err), "task_data": task_data}, flow_id=flow_id, level="ERROR")
+        OpenSearchRepository.log_event(status="FINISHED", content=task_data, flow_id=flow_id, level="INFO")
         return embeddings_vector
